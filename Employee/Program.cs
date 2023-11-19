0f948b734101aeba47318889a67dd51abcde5917
Ssshelesyiuk <118780763+Ssshelesyiuk@users.noreply.github.com>
2023-11-19 15:25:39 +0200

    Update Program.cs

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -2,7 +2,7 @@
 {
     static void Main(string[] args)
     {
-        Employee employee = new Employee("Selyk", "Artem");
+        Employee employee = new Employee("Anny", "Viky");
         employee.DisplayInfo();
         double salary = employee.CalculateSalary(Position.Manager, 8);
         Console.WriteLine($"Salary: {salary} uah");
